3d632fe6669442247e081fb0a23f1e0b2d912426
Plusha30 <plushasstudio@gmail.com>
2026-01-29 17:24:29 +0300

    yeyeyeye

--- promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/app.py
+++ promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/app.py
@@ -54,6 +54,7 @@ app.add_url_rule('/update_inventory', view_func=povar_r.updateinventory, methods
 app.add_url_rule('/buy_to_admin', view_func=povar_r.buy_to_admin, methods=['POST'])
 #admin_routes.py
 app.add_url_rule('/set_admin_query', view_func=admin_r.set_admin_query, methods=['POST'])
+app.add_url_rule('/send_global', view_func=admin_r.send_global, methods=['GET', 'POST'])
 
 @app.errorhandler(404)
 def four04(error):
--- promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/subscript/admin_routes.py
+++ promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/subscript/admin_routes.py
@@ -17,3 +17,17 @@ def set_admin_query():
             break
     setquerylist(name="povar_to_admin.json", to=qu)
     return redirect(url_for('dashboard'), 302)
+
+def send_global():
+    email = getlogin()
+    user = getuser(email)
+    if (email == 'placeholder' or user['rights'] != 3):
+        return redirect(url_for('login'), 302)
+    if (request.method == 'POST'):
+        data = request.form.to_dict(flat=False)
+        if (data['commit_type'][0] == 'update_photo'):
+            txt = request.files['global_menu']
+            if (txt.filename != ''):
+                path = f"{base_path}/tovars/global_menu.txt"
+                txt.save(path)
+    return render_template('send_global.html', **commonkwargs(email))
