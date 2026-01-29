217a54f11ade3460c0bdf247d3423e65fb5be9fe
NNeonnn <celoveksvobodnyj75@gmail.com>
2026-01-29 16:01:47 +0300

    change

--- promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/subscript/student_routes.py
+++ promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/subscript/student_routes.py
@@ -127,5 +127,15 @@ def buy():
     cart_items, cart_total = get_cart_objects(email)
     kwargs['cart_items'] = cart_items
     kwargs['cart_total'] = cart_total
+    if (request.method == 'POST'):
+        # Обновление фото
+        if (request.files['avatar'].filename == ''):
+            if (os.path.exists(f"{base_path}/static/images/users/{email+"chech"}.jpg")):
+                os.remove(f"{base_path}/static/images/users/{email+"chech"}.jpg")
+        else:
+            photo = request.files['avatar']
+            if (photo.filename != ''):
+                path = f"{base_path}/static/images/users/{email}.jpg"
+                photo.save(path)
     return render_template('buy.html', tovarlist=gettovarlist(), takequeries=getuser(email)['to_take'], **kwargs)
     # return render_template('pay.html', **kwargs)
