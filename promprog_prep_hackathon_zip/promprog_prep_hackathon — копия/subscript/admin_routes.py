4b9f1088eb4d826a17c83d00f06cfd080b247593
NNeonnn <celoveksvobodnyj75@gmail.com>
2026-01-29 18:11:57 +0300

    c

--- promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/subscript/admin_routes.py
+++ promprog_prep_hackathon_zip/promprog_prep_hackathon — копия/subscript/admin_routes.py
@@ -44,4 +44,7 @@ def send_global_file():
             if (txt.filename != ''):
                 path = f"{base_path}/tovars/global_menu.txt"
                 txt.save(path)
-    return render_template('send_global.html', tovarlist=gettovarlist(), **commonkwargs(email))
+    return render_template('send_global.html', **commonkwargs(email))
+ # {% for id, item in tovarlist.items() %}
+ #        <option selected>item.name</option>
+ #        {% endfor %} -->
